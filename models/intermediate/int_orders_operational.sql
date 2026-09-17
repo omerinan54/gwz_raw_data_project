3c37d723ef794c83213f78b285c1a04002ae196d
inanomr3454@gmail.com <inanomr3454@gmail.com>
2026-09-17 02:34:38 +0000

    feat: finance_Days file created succesfully

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,6 +1,15 @@
-SELECT 
-orders_id , date_date ,
-ROUND((margin + shipping_fee - log_cost - ship_cost), 2) AS operational_margin
-FROM {{ ref('int_orders_margin') }} 
-LEFT JOIN {{ ref('stg_raw__ship') }}  USING(orders_id )
-
+SELECT
+	o.orders_id
+	,o.date_date
+	,ROUND(o.margin + s.shipping_fee - (s.log_cost + s.ship_cost),2) AS operational_margin
+	,o.quantity
+	,o.revenue
+	,o.purchase_cost
+	,o.margin
+	,s.shipping_fee
+	,s.log_cost
+	,s.ship_cost
+FROM {{ref("int_orders_margin")}} o
+LEFT JOIN {{ref("stg_raw__ship")}} s
+	USING(orders_id)
+ORDER BY orders_id desc
