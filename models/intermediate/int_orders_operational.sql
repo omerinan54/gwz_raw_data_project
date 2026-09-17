b9a02461cad567654108ad37a264769667d2bd61
inanomr3454@gmail.com <inanomr3454@gmail.com>
2026-09-17 01:51:32 +0000

    feat: linage görevi tamamlandı

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,4 +1,5 @@
-SELECT *,
+SELECT 
+orders_id , date_date ,
 ROUND((margin + shipping_fee - log_cost - ship_cost), 2) AS operational_margin
 FROM {{ ref('int_orders_margin') }} 
 LEFT JOIN {{ ref('stg_raw__ship') }}  USING(orders_id )
